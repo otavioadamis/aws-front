44624dfa3321c953f5a1ffd93d224a2c5c03b72f
otavio <otavioadamis@gmail.com>
2023-11-26 20:34:14 -0300

    aws-frontend

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const updateProductDescription = document.querySelector('#update-description');
 
 // Function to fetch all products from the server
 async function fetchProducts() {
-  const response = await fetch('http://localhost:3000/products');
+  const response = await fetch('http://3.147.74.177:3000/products');
   const products = await response.json();
 
   products.sort((a, b) => b.price - a.price);
@@ -63,7 +63,7 @@ addProductForm.addEventListener('submit', async event => {
 
 // Function to add a new product
 async function addProduct(name, price, description) {
-  const response = await fetch('http://localhost:3000/products', {
+  const response = await fetch('http://3.147.74.177:3000/products', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -76,7 +76,7 @@ async function addProduct(name, price, description) {
 
 // Function to delete a new product
 async function deleteProduct(id) {
-  const response = await fetch('http://localhost:3000/products/' + id, {
+  const response = await fetch('http://3.147.74.177:3000/products/' + id, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
@@ -95,7 +95,7 @@ async function updateProduct() {
   const price = updateProductPrice.value;
   const description = updateProductDescription.value;
 
-  await fetch(`http://localhost:3000/products/${id}`, {
+  await fetch(`http://3.147.74.177:3000//${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
